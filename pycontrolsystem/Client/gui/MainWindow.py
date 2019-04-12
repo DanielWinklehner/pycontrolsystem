2eff854656aaf10bd09fa286c0d1c91220b11b99
Daniel Winklehner <winklehn@mit.edu>
2019-04-12 08:20:52 -0400

    testing white plot backgrounds -DW

--- pycontrolsystem/Client/gui/MainWindow.py
+++ pycontrolsystem/Client/gui/MainWindow.py
@@ -26,6 +26,11 @@ from .widgets.DateTimePlotWidget import DateTimePlotWidget
 from ..Device import Device
 from ..Channel import Channel
 
+import pyqtgraph as pg
+# Switch PyQtGraph to using white background and black foreground (before creating main window!)
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+
 
 class MainWindow(QMainWindow):
 
--- pycontrolsystem/Client/gui/style/__init__.py
+++ pycontrolsystem/Client/gui/style/__init__.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 import platform
+import os
 
 # Noinspections necessary for PyCharm because installed PyQt5 module is just called 'pyqt'
 # noinspection PyPackageRequirements
 from PyQt5.QtCore import QFile, QTextStream
 
-# import .dark_alt.style_rc
 
+def dark_stylesheet():
 
-def dark_stylesheet():   
-    f = QFile("gui/style/dark_alt/style.qss")
+    f = QFile(os.path.join(os.path.abspath(os.path.dirname(__file__)), "dark_alt/style.qss"))
+
     if not f.exists():
+
         return ""
+
     else:
+
         f.open(QFile.ReadOnly | QFile.Text)
         ts = QTextStream(f)
         stylesheet = ts.readAll()
+
         if platform.system().lower() == 'darwin':  # see issue #12 on github
+
             mac_fix = '''
             QDockWidget::title
             {
@@ -26,4 +32,5 @@ def dark_stylesheet():
             }
             '''
             stylesheet += mac_fix
+
         return stylesheet
